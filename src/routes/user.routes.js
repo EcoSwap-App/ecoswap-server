224e320aa93c5e1113012eeda5e4bdd8e129dc35
jorgeLinares <jorgelinaresrrs306@hotmail.com>
2026-06-17 23:25:46 -0500

    feat: add user profile routes with corresponding controller methods

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,13 +1,15 @@
 import { Router } from 'express';
-import { syncProfile } from '../controllers/user.controller.js';
+import { syncProfile, getUserStats, getUserById, updateProfile } from '../controllers/user.controller.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
-import { getUserStats } from '../controllers/user.controller.js';
 
 const router = Router();
+
+router.get('/:id', getUserById);
 
 router.use(authMiddleware);
 
 router.post('/sync', syncProfile);
 router.get('/stats', getUserStats);
+router.put('/profile', updateProfile);
 
 export default router;
